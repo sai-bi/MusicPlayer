df0b0308cee14ea8fae4394eb4d0443d3c9c1f33
soundsilence <soundsilencebisai@gmail.com>
2014-04-06 00:04:00 +0800

    finish all functions

--- app/src/main/java/com/example/musicplayer/app/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/app/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.musicplayer.app;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +22,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -29,6 +34,9 @@ public class MainActivity extends ActionBarActivity {
     private ArrayList<String> url_list;
     private SQLiteDatabase database;
     private ArrayAdapter<String> song_list_view_adapter;
+    private int current_index;
+    private Boolean is_playing;
+    private ArrayList<Integer> id_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +44,35 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         final Button add_button = (Button) (findViewById(R.id.btn_add));
         final Button list_button = (Button) (findViewById(R.id.btn_play_list));
+        final Button next_button = (Button) (findViewById(R.id.btn_next));
+        final Button pre_button = (Button) (findViewById(R.id.btn_previous));
+        final Button play_button = (Button) (findViewById(R.id.btn_play));
+        final TextView text_view_song_title = (TextView) (findViewById(R.id.text_view_song_title));
+        final TextView text_view_status = (TextView) (findViewById(R.id.text_view_status));
 
+
+        StatusReceiver statusReceiver = new StatusReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("status_message");
+        MainActivity.this.registerReceiver(statusReceiver, filter);
+
+
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, PlayMusic.class);
+        startService(intent);
 
         song_name = "";
         song_url = "";
         song_list = new ArrayList<String>();
         url_list = new ArrayList<String>();
-
-
-        song_list.add("让她降落");
-        url_list.add("http://i.cs.hku.hk/fyp/2013/fyp13027/music/test.mp3");
+        id_list = new ArrayList<Integer>();
+        current_index = -1;
+        is_playing = false;
 
 
         getSongList();
         setUpListView();
+        setUpSearch();
 
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -108,16 +131,130 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        play_button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (song_list.size() == 0)
+                    return;
+                if (is_playing) {
+                    text_view_status.setText("Pause");
+                    is_playing = false;
+                    Intent intent = new Intent();
+                    intent.putExtra("stop", true);
+                    intent.setAction("player_command");
+                    sendBroadcast(intent);
+                    play_button.setBackgroundResource(R.drawable.play_button);
+                    return;
+                } else {
+
+                    is_playing = true;
+                    play_button.setBackgroundResource(R.drawable.pause);
+                    Intent intent = new Intent();
+                    intent.setAction("player_command");
+
+                    if (current_index == -1) {
+                        text_view_status.setText("Loading...");
+                        current_index = 0;
+                        intent.putExtra("url", url_list.get(current_index));
+                        intent.putExtra("title", song_list.get(current_index));
+                        sendBroadcast(intent);
+                        text_view_song_title.setText(song_list.get(current_index));
+                    } else {
+                        text_view_status.setText("Playing...");
+                        intent.putExtra("resume", true);
+                        sendBroadcast(intent);
+                    }
+                }
+            }
+        });
+
+        next_button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (song_list.size() == 0)
+                    return;
+                current_index = current_index + 1;
+                current_index = current_index % song_list.size();
+                Intent intent = new Intent();
+                intent.setAction("player_command");
+                intent.putExtra("url", url_list.get(current_index));
+                intent.putExtra("title", song_list.get(current_index));
+                sendBroadcast(intent);
+                text_view_song_title.setText(song_list.get(current_index));
+                play_button.setBackgroundResource(R.drawable.pause);
+                is_playing = true;
+            }
+        });
+
+        pre_button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (song_list.size() == 0)
+                    return;
+                if (current_index == -1) {
+                    current_index = 0;
+                }
+                current_index = current_index - 1;
+                current_index = (current_index + song_list.size()) % song_list.size();
+                Intent intent = new Intent();
+                intent.setAction("player_command");
+                intent.putExtra("url", url_list.get(current_index));
+                intent.putExtra("title", song_list.get(current_index));
+                sendBroadcast(intent);
+                text_view_song_title.setText(song_list.get(current_index));
+                play_button.setBackgroundResource(R.drawable.pause);
+                is_playing = true;
+            }
+        });
 
     }
 
     public void getSongList() {
         database = openOrCreateDatabase("song.db", Context.MODE_PRIVATE, null);
         try {
+            // add some songs
             database.execSQL("create table SONG_LIST(_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, url TEXT)");
         } catch (Exception e) {
-            // table already exists
+            e.printStackTrace();
         }
+
+        SharedPreferences sp = getSharedPreferences("data",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        int count = sp.getInt("count",1);
+        if(count == 1){
+            String name = "让她降落";
+            String url = "http://i.cs.hku.hk/fyp/2013/fyp13027/music/rang_ta_jiang_luo.mp3";
+            database.execSQL("insert into SONG_LIST values(null,?,?)", new Object[]{name, url});
+
+            name = "优美地低于生活";
+            url = "http://i.cs.hku.hk/fyp/2013/fyp13027/music/you_mei_de.mp3";
+            database.execSQL("insert into SONG_LIST values(null,?,?)", new Object[]{name, url});
+
+            name = "You are not alone";
+            url = "http://i.cs.hku.hk/fyp/2013/fyp13027/music/you_are_not_alone.mp3";
+            database.execSQL("insert into SONG_LIST values(null,?,?)", new Object[]{name, url});
+
+            name = "We are the world";
+            url = "http://i.cs.hku.hk/fyp/2013/fyp13027/music/we_are_the_world.mp3";
+            database.execSQL("insert into SONG_LIST values(null,?,?)", new Object[]{name, url});
+
+            name = "Heal the world";
+            url = "http://i.cs.hku.hk/fyp/2013/fyp13027/music/heal_the_world.mp3";
+            database.execSQL("insert into SONG_LIST values(null,?,?)", new Object[]{name, url});
+
+            name = "Earth song";
+            url = "http://i.cs.hku.hk/fyp/2013/fyp13027/music/earth_song.mp3";
+            database.execSQL("insert into SONG_LIST values(null,?,?)", new Object[]{name, url});
+
+            name = "Viva la vida";
+            url = "http://i.cs.hku.hk/fyp/2013/fyp13027/music/viva_la_vida.mp3";
+            database.execSQL("insert into SONG_LIST values(null,?,?)", new Object[]{name, url});
+        }
+        count = count + 1;
+        editor.putInt("count",count);
+        editor.commit();
+
+
         Cursor c = database.rawQuery("select * from SONG_LIST", null);
         while (c.moveToNext()) {
             int title_index = c.getColumnIndex("title");
@@ -126,6 +263,9 @@ public class MainActivity extends ActionBarActivity {
             int url_index = c.getColumnIndex("url");
             String url = c.getString(url_index);
             url_list.add(url);
+            int id_index = c.getColumnIndex("_id");
+            int id = c.getInt(id_index);
+            id_list.add(id);
         }
     }
 
@@ -134,20 +274,149 @@ public class MainActivity extends ActionBarActivity {
         song_list_view_adapter = new ArrayAdapter<String>(this, R.layout.list_view_song, song_list);
         list_view_song.setAdapter(song_list_view_adapter);
 
-        list_view_song.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        list_view_song.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getBaseContext(), PlayMusic.class);
-                intent.putExtra("url",url_list.get(i));
-                intent.putExtra("title",song_list.get(i));
-                startService(intent);
+                if (i == current_index) {
+                    return;
+                }
+                current_index = i;
+                Intent intent = new Intent();
+                intent.setAction("player_command");
+                intent.putExtra("url", url_list.get(i));
+                intent.putExtra("title", song_list.get(i));
+                sendBroadcast(intent);
+                Log.d("send", "send a broadcast");
+                TextView v = (TextView) (findViewById(R.id.text_view_song_title));
+                v.setText(song_list.get(i));
+                TextView text_view_status = (TextView) (findViewById(R.id.text_view_status));
+                text_view_status.setText("Loading...");
+                Button btn_play = (Button) (findViewById(R.id.btn_play));
+                btn_play.setBackgroundResource(R.drawable.pause);
+                is_playing = true;
+            }
+        });
+
+        list_view_song.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                final int index = i;
+                builder.setTitle("Delete");
+                builder.setMessage("Do you want to delete this song?");
+                builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        if (current_index > index) {
+                            current_index = index - 1;
+                        }
+                        try {
+                            database.execSQL("Delete from SONG_LIST where _id = " + id_list.get(index).toString());
+                            song_list.remove(index);
+                            url_list.remove(index);
+                            id_list.remove(index);
+                            song_list_view_adapter.notifyDataSetChanged();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.create().show();
+                return true;
             }
         });
     }
 
+    public void setUpSearch() {
 
 
+        final Button btn_search = (Button) (findViewById(R.id.btn_search));
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+        final View view_search_song = inflater.inflate(R.layout.search_dialog, null);
+
+        builder.setView(view_search_song)
+                // Add action buttons
+                .setPositiveButton("Search", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        EditText song_title = (EditText) (view_search_song.findViewById(R.id.input_search_song_title));
+                        String song_name;
+                        if (song_title.getText() != null) {
+                            song_name = song_title.getText().toString();
+                        } else {
+                            return;
+                        }
+
+                        final int index = song_list.indexOf(song_name);
+                        if (index == -1) {
+                            AlertDialog.Builder my_builder = new AlertDialog.Builder(MainActivity.this);
+                            my_builder.setTitle("Search: " + song_name);
+                            my_builder.setMessage("Not found!");
+                            my_builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                            my_builder.create().show();
+                        } else {
+                            AlertDialog.Builder my_builder = new AlertDialog.Builder(MainActivity.this);
+                            my_builder.setTitle("Search: " + song_name);
+                            my_builder.setMessage("URL: " + url_list.get(index));
+                            my_builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                            my_builder.setPositiveButton("Play", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Intent intent = new Intent();
+                                    intent.putExtra("url", url_list.get(index));
+                                    intent.setAction("player_command");
+                                    sendBroadcast(intent);
+                                    is_playing = true;
+
+                                    TextView v = (TextView) (findViewById(R.id.text_view_song_title));
+                                    v.setText(song_list.get(index));
+                                    TextView text_view_status = (TextView) (findViewById(R.id.text_view_status));
+                                    text_view_status.setText("Loading...");
+                                    Button btn_play = (Button) (findViewById(R.id.btn_play));
+                                    btn_play.setBackgroundResource(R.drawable.pause);
+                                }
+                            });
+                            my_builder.create().show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        final Dialog dialog_search = builder.create();
+        dialog_search.setCanceledOnTouchOutside(false);
+
+        btn_search.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog_search.show();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -167,6 +436,29 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    class StatusReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int status = intent.getIntExtra("status", 0);
+            // load complete
+            TextView text_view_status = (TextView) (findViewById(R.id.text_view_status));
+            if (status == 0) {
+                text_view_status.setText("Playing...");
+            } else {
+                text_view_status.setText("Fail to load the music...");
+                is_playing = false;
+                Button play_button = (Button) (findViewById(R.id.btn_play));
+                play_button.setBackgroundResource(R.drawable.play_button);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        database.close();
     }
 
 }
--- app/src/main/java/com/example/musicplayer/app/PlayMusic.java
+++ app/src/main/java/com/example/musicplayer/app/PlayMusic.java
@@ -1,40 +1,131 @@
 package com.example.musicplayer.app;
 
 import android.app.IntentService;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 
 import java.io.IOException;
 
-public class PlayMusic extends IntentService {
+public class PlayMusic extends Service {
+
+    private MediaPlayer player;
+    private String url;
+    private String title;
+    private boolean playing_now;
+    private int current_length;
+    private CommandReceiver command_receiver;
 
 
-    public PlayMusic(String name) {
-        super(name);
+    @Override
+    public void onCreate() {
+        player = new MediaPlayer();
+        url = "";
+        title = "";
+        playing_now = false;
+        command_receiver = new CommandReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("player_command");
+        PlayMusic.this.registerReceiver(command_receiver,filter);
     }
-    public PlayMusic(){
-        super("PlayMusic");
+
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    public void playMusic(String url, String title) {
+        player.reset();
+        this.url = url;
+        this.title = title;
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        new AsyncPlayMusicTask().execute();
+    }
+
+    class AsyncPlayMusicTask extends AsyncTask<Void,Void,Void>{
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try{
+                player.setDataSource(url);
+                player.prepare();
+                player.start();
+                Intent intent = new Intent();
+                intent.setAction("status_message");
+                intent.putExtra("status",0);
+                sendBroadcast(intent);
+            } catch (Exception e){
+                e.printStackTrace();
+                Intent intent = new Intent();
+                intent.setAction("status_message");
+                intent.putExtra("status",-1);
+                sendBroadcast(intent);
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result){
+
+        }
+    }
+
+
+    @Override
+    public void onDestroy(){
+        PlayMusic.this.unregisterReceiver(command_receiver);
+        player.stop();
+        super.onDestroy();
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
-        String url = intent.getStringExtra("url");
-        String title = intent.getStringExtra("title");
-        MediaPlayer player = new MediaPlayer();
-
-
-        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        try {
-            player.setDataSource(url);
-            player.prepare();
-            player.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    public IBinder onBind(Intent intent) {
+        return null;
     }
+
+
+    private class CommandReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("receive","receiver a broadcast");
+            Bundle bundle = intent.getExtras();
+            boolean resume = bundle.getBoolean("resume", false);
+            boolean stop = bundle.getBoolean("stop", false);
+            String input_url = bundle.getString("url");
+            String input_title = bundle.getString("title");
+            if (input_url == null) {
+                input_url = "";
+            }
+            if (input_title == null) {
+                input_title = "";
+            }
+            if (stop) {
+                player.pause();
+                current_length = player.getCurrentPosition();
+                return;
+            }
+            if (resume) {
+                player.seekTo(current_length);
+                player.start();
+                return;
+            }
+
+            playMusic(input_url, input_title);
+
+        }
+    }
+
+
 }
